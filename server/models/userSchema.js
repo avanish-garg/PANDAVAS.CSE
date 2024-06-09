b76fd6c633e4e68ff096459baf105d26118c586e
Avanish Garg <avanishgarg6734@gmail.com>
2024-06-09 14:24:15 +0530

    COnfirm password Added in models

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    cpassword: {
+        type: String,
+        required: true
+    },
     role: {
         type: String,
         enum: ['user', 'admin'],
