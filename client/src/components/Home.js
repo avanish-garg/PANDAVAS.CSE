dd4e414c7977aee01d5bb9853b6e89776eb65491
ayushsoni11 <soniayush6441@gmail.com>
2024-06-10 20:55:25 +0530

    Created Navbar page

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <p>WELCOME</p>
+            <h1>Connect, Create AND Collabor8 </h1>
+        </div>
+    )
+}
+
+export default Home
