bf09d42db06425d1b5472129dd6dd734e366dba8
ayushsoni11 <soniayush6441@gmail.com>
2024-06-10 16:40:58 +0530

    Intalled react app

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -64,8 +64,8 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function(next) {
     console.log("Hi i am inside");
     if(this.isModified('password')) {
-        this.password= await bcrypt.hash(this.password, 12);
-        this.cpassword=await bcrypt.hash(this.cpassword, 12);
+        this.password= bcrypt.hash(this.password, 12);
+        this.cpassword= bcrypt.hash(this.cpassword, 12);
     }
     next();
 });
